f9b0b84cc7af2c8f563be82b3c85960760c944e1
Mohamad Arsya Kaukabi <arsyakaukabi@gmail.com>
2023-07-29 03:56:38 +0700

    Style: Update script to use modern JavaScript with comments

--- content_script.js
+++ content_script.js
@@ -1,21 +1,33 @@
-function removeDOMElement(element) {
+// Function to remove a DOM element if it exists
+const removeDOMElement = (element) => {
   if (element) {
     element.remove();
   }
-}
+};
 
-function updateSectionClass() {
+// Function to update the class name of the hidden paywall section
+const updateSectionClass = () => {
+  // Find the hidden paywall section in the DOM
   const sectionElement = document.querySelector('section.paywall.hidden');
+
+  // If the hidden paywall section is found, update its classes
   if (sectionElement) {
-    sectionElement.classList.remove('paywall', 'hidden');
-    sectionElement.classList.add('non-paywall');
+    sectionElement.classList.remove('paywall', 'hidden'); // Remove 'paywall' and 'hidden' classes
+    sectionElement.classList.add('non-paywall'); // Add the 'non-paywall' class
   }
-}
+};
 
-function applyScript() {
+// Function to apply the script
+const applyScript = () => {
+  // Find the 'kompas-paywall' element in the DOM
   const paywall = document.querySelector("kompas-paywall");
-  removeDOMElement(paywall);
-  updateSectionClass();
-}
 
+  // Remove the 'kompas-paywall' element if it exists
+  removeDOMElement(paywall);
+
+  // Update the class name of the hidden paywall section
+  updateSectionClass();
+};
+
+// Call the function to apply the script
 applyScript();
